e3085f0cd3fa5fbeb23a8d26ac99d2157f038511
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-11 14:57:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ function validate(objCaller){
   let user = document.getElementById("user");
   let email = document.getElementById("email");
   
-  for (let i=0;i<arrUsers.length;++i)
-    if (arrUsers[i]===user.value)
-      document.getElementById("feedback-user").styles.display = "true";
+  document.getElementById("feedback-user").style.display = "none";
+  document.getElementById("feedback-email").style.display = "none";
+  document.getElementById("feedback-pass").style.display = "none";
+  
+  for (let i=0;i<arrUsers.length;++i){
+    if (arrUsers[i]===user.value){
+      document.getElementById("feedback-user").style.display = "inline";
+      return false;
+    }
+    if (arrEmails[i]===email.value){
+      document.getElementById("feedback-email").style.display = "inline";
+      return false;
+    }
+  }
+  
+  if (document.getElementById("pass").value === document.getElementById("conf").value)
+    document.getElementById("feedback-pass").style.display = "inline";
   
   return true;
 }
