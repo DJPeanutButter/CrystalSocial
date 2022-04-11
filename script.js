81bb36c9b7acaf680ad0a96d32bbc2f3adf6045e
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-11 14:47:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function validate(objCaller){
-  console.log(objCaller);
-  console.log(arrUsers);
-  console.log(arrEmails);
-  
   // Compare user to users in DB
   // Compare email to emails in DB
   // Compare pass/conf values
   
+  let user = document.getElementById("user");
+  let email = document.getElementById("email");
+  
+  arrUsers.forEach(x => if(x===user.value) console.log("!"));
   
   return true;
 }
