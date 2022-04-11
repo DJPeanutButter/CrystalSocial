b74788eea6f2bc60a9ba2a4024bbbe26247155b8
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-11 13:42:52 -0500

    Update register.php

--- register.php
+++ register.php
@@ -8,6 +8,13 @@
   <body>
     <from action="createAccount.php" onchange="validate(this)">
       <input type="text" name="user" id="user">
+      <label for="user">Username</label>
+      <input type="email" name="email" id="email">
+      <label for="email">Email</label>
+      <input type="password" name="pass" id="pass">
+      <label for="pass">Password</label>
+      <input type="password" name="conf" id="conf">
+      <label for="conf">Confirm</label>
     </from>
   </body>
 </html>
