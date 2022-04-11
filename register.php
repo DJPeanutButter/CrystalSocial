fefb2bbb6e699c662e7d5990fe01730f64be439e
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-11 13:31:42 -0500

    Create register.php

---
+++ register.php
@@ -0,0 +1,10 @@
+<!DocType HTML>
+<html>
+  <head>
+    <title>Register</title>
+    <link rel="stylesheet" src="styles.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+  </body>
+</html>
