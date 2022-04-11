4c347d0eb8e62a9719231d568f9701693d998ccd
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-11 14:28:46 -0500

    Update register.php

--- register.php
+++ register.php
@@ -7,19 +7,30 @@
   </head>
   <body>
     <from action="createAccount.php" onchange="validate(this)">
-      <label for="user">Username</label>
-      <input type="text" name="user" id="user">
-      <br>
-      <label for="email">Email</label>
-      <input type="email" name="email" id="email">
-      <br>
-      <label for="pass">Password</label>
-      <input type="password" name="pass" id="pass">
-      <br>
-      <label for="conf">Confirm</label>
-      <input type="password" name="conf" id="conf">
-      <br>
-      <input type="submit" id="submit" value="Submit">
+      <table>
+        <tr>
+          <td><label for="user">Username</label></td>
+          <td><input type="text" name="user" id="user"></td>
+          <td><span id="feedback-user">User name is already taken.</span></td>
+        </tr>
+        <tr>
+          <td><label for="email">Email</label></td>
+          <td><input type="email" name="email" id="email"></td>
+          <td><span id="feedback-email">eMail has previously been used.</span></td>
+        </tr>
+        <tr>
+          <td><label for="pass">Password</label></td>
+          <td><input type="password" name="pass" id="pass"></td>
+        </tr>
+        <tr>
+          <td><label for="conf">Confirm</label></td>
+          <td><input type="password" name="conf" id="conf"></td>
+          <td><span id="feedback-pass">Passwords do not match.</span></td>
+        </tr>
+        <tr>
+          <td><input type="submit" id="submit" value="Submit"></td>
+        </tr>
+      </table>
     </from>
   </body>
 </html>
